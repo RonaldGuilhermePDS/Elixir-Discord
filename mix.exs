a9e7f9e0b534c25d9bbab86f8027b6c6dfad1fb8
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2022-07-10 00:58:25 -0300

    chore(deps): add mongodb and poolboy

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule ElixirDiscord.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mongodb, "~> 0.5.1"},
+      {:poolboy, "~> 1.5.1"}
     ]
   end
 
